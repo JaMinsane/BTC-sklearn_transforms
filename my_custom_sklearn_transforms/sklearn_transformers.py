aa024e10769a230b66667ceb5f90af9008020584
JaMinsane <46429005+JaMinsane@users.noreply.github.com>
2020-08-21 19:21:22 +0200

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-
+from sklearn.preprocessing import normalize
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -14,3 +14,35 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+    
+#Todos los datos faltantes de los "cursos completados" se transforman a ceros
+class ZeroColumns(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        # Primero copiamos el dataframe de datos de entrada 'X'
+        data = X.copy()
+        # Devolvemos un nuevo dataframe de datos sin las columnas no deseadas
+        for j in range(6):
+            for i in range(len(X)):
+                if not data.at[i, self.columns[j]] >= 0:
+                    data.at[i, self.columns[j]] = 0
+        return data
+
+#Normalizo los datos de Horas y Numero de cursos completados
+class ScaleColumns(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        data = X.copy()
+        min_max_scaler = MinMaxScaler()
+        data[self.columns] = min_max_scaler.fit_transform(data[self.columns])
+        return data
