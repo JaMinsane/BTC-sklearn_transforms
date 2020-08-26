47c5297ecb031f571e6d8fa6882d2933d244074d
JaMinsane <46429005+JaMinsane@users.noreply.github.com>
2020-08-26 22:38:20 +0200

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -42,3 +42,35 @@ class MiceImputation(BaseEstimator, TransformerMixin):
         data = X.copy()
         data = mice(data)
         return data   
+    
+ class UnderOverSampling(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        data = X.copy()
+        count_class_acep, count_class_sosp = data.OBJETIVO.value_counts()
+        df_class_acep = data[data['OBJETIVO'] == 'Aceptado']
+        df_class_sosp = data[data['OBJETIVO'] == 'Sospechoso']
+        
+        #Random under-sampling
+        df_class_acep_under = df_class_acep.sample(count_class_sosp*3)
+        df_test_under = pd.concat([df_class_acep_under, df_class_sosp], axis=0)
+        
+        #Random over-sampling
+        df_class_sosp_over = df_class_sosp.sample(count_class_sosp*3, replace=True)
+        df_test_over = pd.concat([df_class_acep, df_class_sosp_over], axis=0)
+        
+        df_acep = pd.DataFrame(
+        df_test_under[df_test_under['OBJETIVO'] == 'Aceptado']
+        )
+        df_sosp = pd.DataFrame(
+        df_test_over[df_test_over['OBJETIVO'] == 'Sospechoso']
+        )
+
+        frames = [df_acep, df_sosp]
+        df_result = pd.concat(frames)
+        return df_result
